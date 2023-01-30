cc60f4f33c72aeb658a5abeeb46d5a29532dd6c5
AbhiHecker2023 <122548006+AbhiHecker2023@users.noreply.github.com>
2023-01-30 11:13:00 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,11 +1,17 @@
 function ShowScoreboard () {
     OLED.clear()
     OLED.writeStringNewLine("PLayer A: " + PA)
+    OLED.writeStringNewLine("PLayer B: " + PB)
+    OLED.writeStringNewLine("Ties : " + Ties)
+    OLED.writeStringNewLine("Rounds : " + Rounds)
 }
+let Rounds = 0
+let Ties = 0
+let PB = 0
 let PA = 0
 OLED.init(128, 64)
 PA = 0
-let PB = 0
-let Ties = 0
-let Rounds = 0
+PB = 0
+Ties = 0
+Rounds = 0
 OLED.writeStringNewLine("Shall We Play A Game")
