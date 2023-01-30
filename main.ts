e82d03bbed1034628e4d21f6a8a3c7a0ec89a35a
AbhiHecker2023 <122548006+AbhiHecker2023@users.noreply.github.com>
2023-01-30 11:12:11 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,11 @@
-OLED.init(128, 64)
+function ShowScoreboard () {
+    OLED.clear()
+    OLED.writeStringNewLine("PLayer A: " + PA)
+}
 let PA = 0
+OLED.init(128, 64)
+PA = 0
 let PB = 0
 let Ties = 0
 let Rounds = 0
+OLED.writeStringNewLine("Shall We Play A Game")
